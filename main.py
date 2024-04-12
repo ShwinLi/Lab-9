4d41a20bc1f9db76c411d78b5e91faf565239f88
ShwinLi <liandrew0404@gmail.com>
2024-04-11 22:15:24 -0400

    Moved all functions into main

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-from menu import Menu
-from encoder import encoder
+def encoder(password_to_encode): # this encodes stuff
+    encoded_password = list(str(password_to_encode))
+
+    for i in range(len(encoded_password)):
+        if int(encoded_password[i]) < 7:
+            encoded_password[i] = int(encoded_password[i]) + 3
+
+        elif int(encoded_password[i]) == 7:
+            encoded_password[i] = 0
+
+        elif int(encoded_password[i]) == 8:
+            encoded_password[i] = 1
+
+        elif int(encoded_password[i]) == 9:
+            encoded_password[i] = 2
+
+    for i in range(len(encoded_password)):
+        encoded_password[i] = str(encoded_password[i])
+
+    return ''.join(encoded_password)
+
+def Menu(): # prints the menu
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
 
 if __name__ == "__main__":
     while True:
